297ecde3516ee7009f2a36f282c28de6a0fe4eb4
Rich Zhao <zhao.zhenhua@qq.com>
2022-01-07 00:27:30 +0800

    change to modules

--- nvim/lua/modules/lsp/init.lua
+++ nvim/lua/modules/lsp/init.lua
@@ -3,5 +3,5 @@ if not status_ok then
 	return
 end
 
-require("rich.lsp.lsp-installer")
-require("rich.lsp.handlers").setup()
+require("modules.lsp.lsp-installer")
+require("modules.lsp.handlers").setup()
