b06c88e288017e1dbadd065530ce5751deae7fdb
Rich Zhao <zhao.zhenhua@qq.com>
2022-01-12 23:18:39 +0800

    lspconfig for sumneko_lua

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -5,18 +5,18 @@ M.setup_lsp = function(attach, capabilities)
 
    -- lspservers with default config
 
-    -- local servers = { "gopls", "sumneko_lua" }
-    local servers = { "gopls" }
+  local servers = { "gopls", "sumneko_lua" }
+   -- local servers = { "gopls" }
 
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-         on_attach = attach,
-         capabilities = capabilities,
-         flags = {
-            debounce_text_changes = 150,
-         },
-      }
-   end
+  -- for _, lsp in ipairs(servers) do
+  --     lspconfig[lsp].setup {
+  --        on_attach = attach,
+  --        capabilities = capabilities,
+  --        flags = {
+  --           debounce_text_changes = 150,
+  --        },
+  --     }
+  -- end
    
   lspconfig.gopls.setup {
     cmd = {"gopls", "serve"},
@@ -30,7 +30,26 @@ M.setup_lsp = function(attach, capabilities)
     },
   }
 
--- the above tsserver lspconfig will remvoe the tsserver's inbuilt formatting,since I use null-ls with denofmt for formatting ts/js.
+  local sumneko_root_path = "/usr/local/bin/lua-language-server/"
+  local sumneko_binary = "/usr/local/bin/lua-language-server/bin/lua-language-server"
+
+  require'lspconfig'.sumneko_lua.setup {
+    cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+    settings = {
+      Lua = {
+        runtime = {
+          version = "Lua 5.3",
+          path = vim.split(package.path, ';')
+          },
+        diagnostics = {
+          globals = {"vim"},
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file(';', true),
+        },
+      },
+    },
+  }
 end
 
 return M
