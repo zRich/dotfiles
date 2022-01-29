44a8082ded5a2bbcb99929ac3f5536737e9cbee8
Rich Zhao <zhao.zhenhua@qq.com>
2022-01-29 22:41:27 +0800

    kepmapping

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -8,6 +8,7 @@ local map = require("core.utils").map
 
 map("n", "<leader>cc", ":Telescope <CR>")
 map("n", "<leader>q", ":q <CR>")
+-- map("n", "<leader>fm", ":lua vim.lsp.buf.formatting()<CR>")
 -- NOTE: the 4th argument in the map function can be a table i.e options but its most likely un-needed so dont worry about it
 
 -- Install plugins
@@ -42,6 +43,24 @@ customPlugins.add(function(use)
   use {
     'ishan9299/nvim-solarized-lua'
   }
+
+  use {
+    'leafgarland/typescript-vim'
+  }
+
+  use {
+      "jose-elias-alvarez/null-ls.nvim",
+      after = "nvim-lspconfig",
+      config = function()
+         require("custom.plugins.null-ls").setup()
+      end,
+   }
+
+  use {
+    'sbdchd/neoformat'
+  }
+-- load it after nvim-lspconfig , since we'll use some lspconfig stuff in the null-ls config!
+
  end)
 
 -- NOTE: we heavily suggest using Packer's lazy loading (with the 'event','cmd' fields)
